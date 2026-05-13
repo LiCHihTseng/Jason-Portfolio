498cfcc2c6c6e45b53812faf7df8e396d12e706d
LiChihTseng <lichih.tseng@uqconnect.edu.au>
2026-05-13 15:39:50 +0800

    update content

--- src/pages/ProjectDetail_Chatstat.jsx
+++ src/pages/ProjectDetail_Chatstat.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import chatstat_banner from "../assets/img/chatstat_Banner.png";
+import chatstat_banner from "../assets/img/Chatstat_Banner.png";
 
 const ProjectDetail_Chatstat = () => {
   const project = {
